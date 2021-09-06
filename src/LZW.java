5f4549823c660a44ff48cafe3419ef36e10a72d1
Alex-Shane <ashane1230@gmail.com>
2021-09-05 20:08:29 -0700

    Compress method compiles and outputs list of map indexes to a .txt file. Unsure if encoded properly.

--- src/LZW.java
+++ src/LZW.java
@@ -1,12 +1,16 @@
 import java.util.*;
+import java.io.*;
 public class LZW {
 	
 	private HashMap <String, Integer> dict;
-	
-	public LZW ()
+	private File orginalFile;
+	private int dictLength;
+	public LZW (File original)
 	{
+		orginalFile = original;
 		this.dict = new HashMap <String,Integer>();
-		for (int k = 0; k < 256; k++)
+		dictLength = 256;
+		for (int k = 0; k < dictLength; k++)
 		{
 			dict.put("" + (char)k, k);
 		}
@@ -17,14 +21,53 @@ public class LZW {
 		return dict;
 	}
 	
-	public static void main (String [] args)
+	public void compress () throws IOException
 	{
-		LZW l = new LZW();
-		HashMap<String,Integer> test = l.getMap();
-		for (int i = 0; i < 256; i++)
+		try 
+		{
+			BufferedReader br = new BufferedReader (new FileReader(orginalFile));
+			BufferedWriter fileWriter = new BufferedWriter(new FileWriter("/Users/Alex/Desktop/Advanced-Topics-CS/LZW/compressedFile.txt"));
+			String current = ""+ (char)br.read();
+			br.mark(100);
+			while (br.read() != -1)
+			{
+				br.reset();
+				String next = "" + (char)br.read();
+				if (dict.containsKey(current+next))
+				{
+					current = current + next;
+				}
+				else
+				{
+					fileWriter.write(dict.get(current) + " ");
+					System.out.println (current);
+					dict.put (current+next,dictLength+1);
+					current = next;
+					dictLength++;
+				}
+				br.mark(100);
+			}
+			System.out.println (dict.get(current));
+			fileWriter.write(""+dict.get(current));
+			fileWriter.close();
+			br.close();
+			System.out.println ("File successfully compressed");
+		} 
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main (String [] args) throws IOException
+	{
+		File f = new File ("/Users/Alex/Desktop/Advanced-Topics-CS/LZW/lzw-file1.txt");
+		LZW l = new LZW(f);
+		//HashMap<String,Integer> test = l.getMap();
+		/*for (int i = 0; i < 256; i++)
 		{
 			System.out.println (""+(char)i + ", " + test.get("" + (char)i)); 
-		}
+		}*/
+		l.compress();
 		
 	}
 }
