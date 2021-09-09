236ce58a09104b624fcdb741635dfcf55ee895d4
Alex-Shane <ashane1230@gmail.com>
2021-09-08 20:22:21 -0700

    All files compressed. fromAscii method is a godsend please use.

--- src/LZW.java
+++ src/LZW.java
@@ -1,7 +1,23 @@
 import java.util.*;
 import java.io.*;
 public class LZW {
-	
+	private static final byte[] EMPTY_BYTE_ARRAY = new byte[0];
+	private static final int BIT_0 = 1;
+	/** Mask for bit 1 of a byte. */
+	private static final int BIT_1 = 0x02;
+	/** Mask for bit 2 of a byte. */
+	private static final int BIT_2 = 0x04;
+	/** Mask for bit 3 of a byte. */
+	private static final int BIT_3 = 0x08;
+	/** Mask for bit 4 of a byte. */
+	private static final int BIT_4 = 0x10;
+	/** Mask for bit 5 of a byte. */
+	private static final int BIT_5 = 0x20;
+	/** Mask for bit 6 of a byte. */
+	private static final int BIT_6 = 0x40;
+	/** Mask for bit 7 of a byte. */
+	private static final int BIT_7 = 0x80;
+	private static final int[] BITS = { BIT_0, BIT_1, BIT_2, BIT_3, BIT_4, BIT_5, BIT_6, BIT_7 };
 	private HashMap <String, Integer> dict;
 	private File orginalFile;
 	private int dictLength;
@@ -26,8 +42,10 @@ public class LZW {
 		try 
 		{
 			BufferedReader br = new BufferedReader (new FileReader(orginalFile));
-			FileOutputStream fileWriter = new FileOutputStream("/Users/Alex/Desktop/Advanced-Topics-CS/LZW/testResult.bin");
+			FileOutputStream fileWriter = new FileOutputStream("/Users/Alex/Desktop/Advanced-Topics-CS/LZW/compressedFile.bin");
 			String current = ""+ (char)br.read();
+			int asciiVal;
+			String binaryString = "";
 			br.mark(100);
 			while (br.read() != -1)
 			{
@@ -39,32 +57,63 @@ public class LZW {
 				}
 				else
 				{
-					byte[] currentByte = current.getBytes();
-					fileWriter.write(currentByte);
-					//fileWriter.write(dict.get(current) + " ");
-					//System.out.println (current);
+					asciiVal = dict.get(current);
+					binaryString += LZW.toBinary(asciiVal, 8);
 					dict.put (current+next,dictLength);//not dictLength+1 because dictLength is always one value greater than dictionary index.
 					current = next;
 					dictLength++;
 				}
 				br.mark(100);
 			}
-			byte[] lastByte = current.getBytes();
-			System.out.println (dict.get(current));
-			fileWriter.write(lastByte);
-			//fileWriter.write(""+dict.get(current));
+			asciiVal = dict.get(current);
+			binaryString += LZW.toBinary(asciiVal, 8);
+			char[] encodedChars = binaryString.toCharArray();
+			byte[] encodedBytes = LZW.fromAscii(encodedChars);
+			fileWriter.write(encodedBytes);
 			fileWriter.close();
 			br.close();
-			System.out.println ("File successfully compressed");
+			System.out.println ("File compressed");
 		} 
 		catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
 	
+	public static String toBinary(int x, int len)
+    {
+        if (len > 0)
+        {
+            return String.format("%" + len + "s",
+                            Integer.toBinaryString(x)).replaceAll(" ", "0");
+        }
+ 
+        return null;
+    }
+	
+	//method below taken from internet;
+	public static byte[] fromAscii(char[] ascii) {
+	    if (ascii == null || ascii.length == 0) {
+	      return EMPTY_BYTE_ARRAY;
+	    }
+	    // get length/8 times bytes with 3 bit shifts to the right of the length
+	    byte[] l_raw = new byte[ascii.length >> 3];
+	    /*
+	     * We decrease index jj by 8 as we go along to not recompute indices using
+	     * multiplication every time inside the loop.
+	     */
+	    for (int ii = 0, jj = ascii.length - 1; ii < l_raw.length; ii++, jj -= 8) {
+	      for (int bits = 0; bits < BITS.length; ++bits) {
+	        if (ascii[jj - bits] == '1') {
+	          l_raw[ii] |= BITS[bits];
+	        }
+	      }
+	    }
+	    return l_raw;
+	  }
+	
 	public static void main (String [] args) throws IOException
 	{
-		File f = new File ("/Users/Alex/Desktop/Advanced-Topics-CS/LZW/test.txt");
+		File f = new File ("/Users/Alex/Desktop/Advanced-Topics-CS/LZW/lzw-file3.txt");
 		LZW l = new LZW(f);
 		l.compress();
 		
